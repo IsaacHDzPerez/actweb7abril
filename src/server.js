a52f7a4e7f5e70e6eb6260848ab69ce2aba4b710
IsaacHDzPerez <A01198674@tec.mx>
2025-04-07 21:18:41 -0600

    actchanges

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const studentRoutes = require('./src/routes/costumers');
+// const errorMiddleware = require('./src/middleware/error');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.use('/api/students', studentRoutes);
+// app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000;
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
